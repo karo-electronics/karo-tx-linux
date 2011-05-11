8d3220d536250528768751bcdf5256a1559cdb2e
Sascha Hauer <s.hauer@pengutronix.de>
2011-05-11 11:31:54 +0200

    ARM i.MX: get rid of wrong MXC_INTERNAL_IRQ usage
    There are several occurences where MXC_INTERNAL_IRQ is
    assumed to be the start of the gpio interrupts. It was never
    meant this way. Replace these with gpio_to_irq.
    
    Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>
    Acked-by: Fabio Estevam <fabio.estevam@freescale.com>

--- arch/arm/mach-imx/mach-mx27_3ds.c
+++ arch/arm/mach-imx/mach-mx27_3ds.c
@@ -45,7 +45,7 @@
 #define SD1_EN_GPIO (GPIO_PORTB + 25)
 #define OTG_PHY_RESET_GPIO (GPIO_PORTB + 23)
 #define SPI2_SS0 (GPIO_PORTD + 21)
-#define EXPIO_PARENT_INT	(MXC_INTERNAL_IRQS + GPIO_PORTC + 28)
+#define EXPIO_PARENT_INT	gpio_to_irq(IMX_GPIO_NR(3, 28))
 
 static const int mx27pdk_pins[] __initconst = {
 	/* UART1 */
