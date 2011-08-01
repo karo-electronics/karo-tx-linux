bdd12814526c603e2b867742e9ff2db148435b45
Stephen Rothwell <sfr@canb.auug.org.au>
2011-08-01 14:08:27 +1000

    Revert "kdb: Remove all references to DOING_KGDB2"
    This reverts commit 621ebf205b994af7998e6d2f9628f238998812c9.

--- kernel/debug/kdb/kdb_debugger.c
+++ kernel/debug/kdb/kdb_debugger.c
@@ -127,8 +127,10 @@ int kdb_stub(struct kgdb_state *ks)
 	KDB_STATE_CLEAR(PAGER);
 	kdbnearsym_cleanup();
 	if (error == KDB_CMD_KGDB) {
-		if (KDB_STATE(DOING_KGDB))
+		if (KDB_STATE(DOING_KGDB) || KDB_STATE(DOING_KGDB2)) {
 			KDB_STATE_CLEAR(DOING_KGDB);
+			KDB_STATE_CLEAR(DOING_KGDB2);
+		}
 		return DBG_PASS_EVENT;
 	}
 	kdb_bp_install(ks->linux_regs);
