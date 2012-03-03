dfb3cc6a0b22497fdcc94ad18799560edb818bd5
Peter Zijlstra <a.p.zijlstra@chello.nl>
2012-03-03 17:05:16 +0100

    sched, numa, mm: Introduce tsk_home_node()
    Introduce the home-node concept for tasks. In order to keep memory
    locality we need to have a something to stay local to, we define the
    home-node of a task as the node we prefer to allocate memory from and
    prefer to execute on.
    
    These are no hard guarantees, merely soft preferences. This allows for
    optimal resource usage, we can run a task away from the home-node, the
    remote memory hit -- while expensive -- is less expensive than not
    running at all, or very little, due to severe cpu overload.
    
    Similarly, we can allocate memory from another node if our home-node
    is depleted, again, some memory is better than no memory.
    
    This patch merely introduces the basic infrastructure, all policy
    comes later.
    
    NOTE: we introduce the concept of EMBEDDED_NUMA, these are
    architectures where the memory access cost doesn't depend on the cpu
    but purely on the physical address -- embedded boards with cheap
    (slow) and expensive (fast) memory banks.
    
    Signed-off-by: Peter Zijlstra <a.p.zijlstra@chello.nl>
    Cc: Lee Schermerhorn <Lee.Schermerhorn@hp.com>
    Cc: Rik van Riel <riel@redhat.com>
    Cc: Andrew Morton <akpm@linux-foundation.org>
    Cc: Linus Torvalds <torvalds@linux-foundation.org>
    Link: http://lkml.kernel.org/n/tip-ii8j8cp87cgctecfqp2ib6rn@git.kernel.org
    Signed-off-by: Ingo Molnar <mingo@kernel.org>

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -1479,6 +1479,9 @@ struct task_struct {
 	short il_next;
 	short pref_node_fork;
 #endif
+#ifdef CONFIG_SCHED_NUMA
+	int node;
+#endif
 	struct rcu_head rcu;
 
 	/*
@@ -1552,6 +1555,15 @@ struct task_struct {
 
 /* Future-safe accessor for struct task_struct's cpus_allowed. */
 #define tsk_cpus_allowed(tsk) (&(tsk)->cpus_allowed)
+
+static inline int tsk_home_node(struct task_struct *p)
+{
+#ifdef CONFIG_SCHED_NUMA
+	return p->node;
+#else
+	return -1;
+#endif
+}
 
 /*
  * Priority of a process goes from 0..MAX_PRIO-1, valid RT
