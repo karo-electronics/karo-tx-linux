6a5ece28208bba32b2f24860d7be0b2f107778df
Minchan Kim <minchan@kernel.org>
2012-09-07 10:23:55 +1000

    mm: change enum migrate_mode with bitwise type
    Change migrate_mode type to bitwise type because next patch will add
    MIGRATE_DISCARD and it could be ORed with other attributes so it would be
    better to change it with bitwise type.
    
    Suggested by Michal Nazarewicz.
    
    Signed-off-by: Minchan Kim <minchan@kernel.org>
    Acked-by: Michal Nazarewicz <mina86@mina86.com>
    Cc: Rik van Riel <riel@redhat.com>
    Cc: Mel Gorman <mgorman@suse.de>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- include/linux/fs.h
+++ include/linux/fs.h
@@ -637,7 +637,7 @@ struct address_space_operations {
 	 * is false, it must not block.
 	 */
 	int (*migratepage) (struct address_space *,
-			struct page *, struct page *, enum migrate_mode);
+			struct page *, struct page *, migrate_mode_t);
 	int (*launder_page) (struct page *);
 	int (*is_partially_uptodate) (struct page *, read_descriptor_t *,
 					unsigned long);
@@ -2734,7 +2734,7 @@ extern int generic_check_addressable(unsigned, u64);
 #ifdef CONFIG_MIGRATION
 extern int buffer_migrate_page(struct address_space *,
 				struct page *, struct page *,
-				enum migrate_mode);
+				migrate_mode_t);
 #else
 #define buffer_migrate_page NULL
 #endif
