83babc0d2944e2c72063a83f4acf3689c071afdf
Ingo Molnar <mingo@kernel.org>
2012-09-26 11:48:08 +0200

    mm/pgprot: Move the pgprot_modify() fallback definition to mm.h
    pgprot_modify() is available on x86, but on other architectures it only
    gets defined in mm/mprotect.c - breaking the build if anything outside
    of mprotect.c tries to make use of this function.
    
    Move it to the generic pgprot area in mm.h, so that an upcoming patch
    can make use of it.
    
    Acked-by: Peter Zijlstra <a.p.zijlstra@chello.nl>
    Cc: Rik van Riel <riel@redhat.com>
    Cc: Paul Turner <pjt@google.com>
    Cc: Linus Torvalds <torvalds@linux-foundation.org>
    Cc: Andrew Morton <akpm@linux-foundation.org>
    Link: http://lkml.kernel.org/n/tip-nfvarGMj9gjavowroorkizb4@git.kernel.org
    Signed-off-by: Ingo Molnar <mingo@kernel.org>

--- include/linux/mm.h
+++ include/linux/mm.h
@@ -164,6 +164,19 @@ extern pgprot_t protection_map[16];
 #define FAULT_FLAG_TRIED	0x40	/* second try */
 
 /*
+ * Some architectures (such as x86) may need to preserve certain pgprot
+ * bits, without complicating generic pgprot code.
+ *
+ * Most architectures don't care:
+ */
+#ifndef pgprot_modify
+static inline pgprot_t pgprot_modify(pgprot_t oldprot, pgprot_t newprot)
+{
+	return newprot;
+}
+#endif
+
+/*
  * vm_fault is filled by the the pagefault handler and passed to the vma's
  * ->fault function. The vma's ->fault is responsible for returning a bitmask
  * of VM_FAULT_xxx flags that give details about how the fault was handled.
