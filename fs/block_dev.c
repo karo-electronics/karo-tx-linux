37f5d2f5c25572884dbe7ef344efd03b04c45d77
Stephen Rothwell <sfr@canb.auug.org.au>
2012-10-02 16:02:56 +1000

    prio_tree: fix fs/block_dev.c for removal of prio_tree
    Signed-off-by: Stephen Rothwell <sfr@canb.auug.org.au>
    Cc: Michel Lespinasse <walken@google.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- fs/block_dev.c
+++ fs/block_dev.c
@@ -27,6 +27,7 @@
 #include <linux/namei.h>
 #include <linux/log2.h>
 #include <linux/cleancache.h>
+#include <linux/rbtree.h>
 #include <asm/uaccess.h>
 #include "internal.h"
 
@@ -132,7 +133,7 @@ int set_blocksize(struct block_device *bdev, int size)
 	/* Check that the block device is not memory mapped */
 	mapping = bdev->bd_inode->i_mapping;
 	mutex_lock(&mapping->i_mmap_mutex);
-	if (!prio_tree_empty(&mapping->i_mmap) ||
+	if (!RB_EMPTY_ROOT(&mapping->i_mmap) ||
 	    !list_empty(&mapping->i_mmap_nonlinear)) {
 		mutex_unlock(&mapping->i_mmap_mutex);
 		percpu_up_write(&bdev->bd_block_size_semaphore);
