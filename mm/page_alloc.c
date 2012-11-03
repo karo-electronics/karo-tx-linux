1ec658c811ea58eb358096e1b93ea08fcf411148
Michal Hocko <mhocko@suse.cz>
2012-11-03 11:42:18 +1100

    mm: make zone_pcp_reset independent of MEMORY_HOTREMOVE
    340175b7 (mm/hotplug: free zone->pageset when a zone becomes empty)
    introduced zone_pcp_reset and hided it inside CONFIG_MEMORY_HOTREMOVE.
    Since "memory-hotplug: allocate zone's pcp before onlining pages" the
    function is also called from online_pages which is defined outside
    CONFIG_MEMORY_HOTREMOVE which causes a linkage error.
    
    The function, although not used outside of MEMORY_{HOTPLUT,HOTREMOVE},
    seems like universal enough so let's keep it at its current location
    and only remove the HOTREMOVE guard.
    
    Signed-off-by: Michal Hocko <mhocko@suse.cz>
    Reviewed-by: Wen Congyang <wency@cn.fujitsu.com>
    Cc: David Rientjes <rientjes@google.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- mm/page_alloc.c
+++ mm/page_alloc.c
@@ -5981,7 +5981,6 @@ void __meminit zone_pcp_update(struct zone *zone)
 }
 #endif
 
-#ifdef CONFIG_MEMORY_HOTREMOVE
 void zone_pcp_reset(struct zone *zone)
 {
 	unsigned long flags;
@@ -6001,6 +6000,7 @@ void zone_pcp_reset(struct zone *zone)
 	local_irq_restore(flags);
 }
 
+#ifdef CONFIG_MEMORY_HOTREMOVE
 /*
  * All pages in the range must be isolated before calling this.
  */
