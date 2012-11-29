3ee00d64888808bf107d34607115f57e74879f29
Andrew Morton <akpm@linux-foundation.org>
2012-11-29 14:18:08 +1100

    mm: add a reminder comment for __GFP_BITS_SHIFT
    Cc: Glauber Costa <glommer@parallels.com>
    Cc: Mel Gorman <mgorman@suse.de>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- include/linux/gfp.h
+++ include/linux/gfp.h
@@ -34,6 +34,7 @@ struct vm_area_struct;
 #define ___GFP_OTHER_NODE	0x200000u
 #define ___GFP_WRITE		0x400000u
 #define ___GFP_KMEMCG		0x800000u
+/* If the above are modified, __GFP_BITS_SHIFT may need updating */
 
 /*
  * GFP bitmasks..
