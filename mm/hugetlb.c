cbc457b96d83c98bc815bcea313d8d95c9742f8e
Andrew Morton <akpm@linux-foundation.org>
2013-03-20 15:06:46 +1100

    mm-hugetlb-fix-total-hugetlbfs-pages-count-when-memory-overcommit-accouting-fix
    coding-style tweak
    
    Cc: Wanpeng Li <liwanp@linux.vnet.ibm.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- mm/hugetlb.c
+++ mm/hugetlb.c
@@ -2126,6 +2126,7 @@ unsigned long hugetlb_total_pages(void)
 {
 	struct hstate *h;
 	unsigned long nr_total_pages = 0;
+
 	for_each_hstate(h)
 		nr_total_pages += h->nr_huge_pages * pages_per_huge_page(h);
 	return nr_total_pages;
