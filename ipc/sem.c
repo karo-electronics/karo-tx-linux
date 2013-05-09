a20229945ef7bf93ac809e88d0ccbd72cbf800a0
Andrew Morton <akpm@linux-foundation.org>
2013-05-10 09:57:04 +1000

    ipcsem-fix-semctl-getzcnt-fix
    checkpatch fixes
    
    Cc: Rik van Riel <riel@redhat.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- ipc/sem.c
+++ ipc/sem.c
@@ -823,7 +823,8 @@ static int count_semzcnt (struct sem_array * sma, ushort semnum)
 
 	semzcnt = 0;
 	list_for_each_entry(q, &sma->sem_base[semnum].sem_pending, list) {
-		struct sembuf * sops = q->sops;
+		struct sembuf *sops = q->sops;
+
 		BUG_ON(sops->sem_num != semnum);
 		if ((sops->sem_op == 0) && !(sops->sem_flg & IPC_NOWAIT))
 			semzcnt++;
