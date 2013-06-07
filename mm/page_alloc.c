02348ead5c9ab43de5a905073c05b26bcd5f8a40
Jiang Liu <liuj97@gmail.com>
2013-06-07 10:08:01 +1000

    mm-correctly-update-zone-managed_pages-fix-fix
    When CONFIG_HIGHMEM is undefined, totalhigh_pages is defined as:
    #define totalhigh_pages 0UL
    Thus statement "totalhigh_pages += count" will cause build failure as:
      CC      mm/page_alloc.o
    mm/page_alloc.c: In function `adjust_managed_page_count':
    mm/page_alloc.c:5262:19: error: lvalue required as left operand of
    assignment
    make[1]: *** [mm/page_alloc.o] Error 1
    make: *** [mm/page_alloc.o] Error 2
    
    So we still need to use CONFIG_HIGHMEM to guard the statement.
    
    Cc: Michal Hocko <mhocko@suse.cz>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- mm/page_alloc.c
+++ mm/page_alloc.c
@@ -5197,8 +5197,10 @@ void adjust_managed_page_count(struct page *page, long count)
 	spin_lock(&managed_page_count_lock);
 	page_zone(page)->managed_pages += count;
 	totalram_pages += count;
+#ifdef CONFIG_HIGHMEM
 	if (PageHighMem(page))
 		totalhigh_pages += count;
+#endif
 	spin_unlock(&managed_page_count_lock);
 }
 EXPORT_SYMBOL(adjust_managed_page_count);
