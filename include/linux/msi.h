b7d406d03322ee6ccb69188744116dda581bf561
Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
2013-08-09 22:27:09 +0200

    of: pci: add registry of MSI chips
    This commit adds a very basic registry of msi_chip structures, so that
    an IRQ controller driver can register an msi_chip, and a PCIe host
    controller can find it, based on a 'struct device_node'.
    
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
    Acked-by: Benjamin Herrenschmidt <benh@kernel.crashing.org>
    Acked-by: Rob Herring <rob.herring@calxeda.com>
    Signed-off-by: Jason Cooper <jason@lakedaemon.net>

--- include/linux/msi.h
+++ include/linux/msi.h
@@ -83,6 +83,8 @@ struct msi_chip {
 struct msi_chip {
 	struct module *owner;
 	struct device *dev;
+	struct device_node *of_node;
+	struct list_head list;
 
 	int (*setup_irq)(struct msi_chip *chip, struct pci_dev *dev,
 			 struct msi_desc *desc);
