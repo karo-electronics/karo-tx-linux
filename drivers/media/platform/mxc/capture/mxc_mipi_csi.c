33bb4f2a2e2321387348f3a74a39e718cd26a5cc
Markus Bauer <mb@karo-electronics.de>
2021-02-02 10:11:36 +0100

    media: mxc: msc_mipi_csi: revert changes added with imx219 driver

--- drivers/media/platform/mxc/capture/mxc_mipi_csi.c
+++ drivers/media/platform/mxc/capture/mxc_mipi_csi.c
@@ -322,10 +322,6 @@ static const struct csis_pix_format mipi_csis_formats[] = {
 		.code = MEDIA_BUS_FMT_SBGGR8_1X8,
 		.fmt_reg = MIPI_CSIS_ISPCFG_FMT_RAW8,
 		.data_alignment = 8,
-	}, {
-		.code = MEDIA_BUS_FMT_SRGGB8_1X8,
-		.fmt_reg = MIPI_CSIS_ISPCFG_FMT_RAW8,
-		.data_alignment = 8,
 	}
 };
 
