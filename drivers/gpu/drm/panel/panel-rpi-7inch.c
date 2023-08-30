095fd85946837ff8619417dbc833aa38b9f8ecd0
Markus Bauer <mb@karo-electronics.de>
2023-08-30 13:58:15 +0200

    rpi panel: switch to version using regulator

--- drivers/gpu/drm/panel/panel-rpi-7inch.c
+++ drivers/gpu/drm/panel/panel-rpi-7inch.c
@@ -1,9 +1,7 @@
 // SPDX-License-Identifier: GPL-2.0
 /*
- * Copyright (C) 2020 Markus Bauer <MB@karo-electronics.de>
- *
- * based on: drivers/gpu/drm/panel/panel-rpi-7inch.c
- * from: https://github.com/TinkerEdgeT/mendel-linux-imx
+ * Copyright (C) 2021, Markus Bauer <mb@karo-electronics.de>
+ * Edited and fitted to our needs.
  *
  * Copyright (C) 2013, NVIDIA Corporation.  All rights reserved.
  *
@@ -29,17 +27,16 @@
 
 #include <linux/delay.h>
 #include <linux/gpio/consumer.h>
-#include <linux/media-bus-format.h>
+#include <linux/i2c.h>
 #include <linux/module.h>
 #include <linux/of_platform.h>
 #include <linux/platform_device.h>
 #include <linux/regulator/consumer.h>
 
+#include <drm/drm_print.h>
 #include <drm/drm_crtc.h>
 #include <drm/drm_mipi_dsi.h>
 #include <drm/drm_panel.h>
-#include <drm/drm_print.h>
-#include <drm/rpi_display.h>
 
 #include <video/display_timing.h>
 #include <video/of_display_timing.h>
@@ -70,8 +67,6 @@
 /* System Controller Registers */
 #define SYSCTRL			0x0464
 
-static int trigger_bridge = 1;
-
 struct panel_desc {
 	const struct drm_display_mode *modes;
 	unsigned int num_modes;
@@ -84,24 +79,6 @@ struct panel_desc {
 		unsigned int width;
 		unsigned int height;
 	} size;
-
-	/**
-	 * @prepare: the time (in milliseconds) that it takes for the panel to
-	 *		become ready and start receiving video data
-	 * @enable: the time (in milliseconds) that it takes for the panel to
-	 *		display the first valid frame after starting to receive
-	 *		video data
-	 * @disable: the time (in milliseconds) that it takes for the panel to
-	 *		turn the display off (no content is visible)
-	 * @unprepare: the time (in milliseconds) that it takes for the panel
-	 *		to power itself down completely
-	 */
-	struct {
-		unsigned int prepare;
-		unsigned int enable;
-		unsigned int disable;
-		unsigned int unprepare;
-	} delay;
 
 	u32 bus_format;
 };
@@ -116,8 +93,10 @@ struct tc358762 {
 	const struct panel_desc *desc;
 
 	struct regulator *supply;
+	struct i2c_adapter *ddc;
 
 	struct gpio_desc *enable_gpio;
+	struct regulator *power_supply;
 };
 
 static const struct drm_display_mode tc358762_mode = {
@@ -145,12 +124,7 @@ static int tc358762_disable(struct drm_panel *panel)
 	if (!p->enabled)
 		return 0;
 
-	pr_debug("panel disable\n");
-
-	rpi_display_set_bright(0x00);
-
-	if (p->desc && p->desc->delay.disable)
-		msleep(p->desc->delay.disable);
+	regulator_disable(p->power_supply);
 
 	p->enabled = false;
 
@@ -164,14 +138,6 @@ static int tc358762_unprepare(struct drm_panel *panel)
 	if (!p->prepared)
 		return 0;
 
-	if (p->enable_gpio)
-		gpiod_direction_output(p->enable_gpio, 0);
-
-	regulator_disable(p->supply);
-
-	if (p->desc && p->desc->delay.unprepare)
-		msleep(p->desc->delay.unprepare);
-
 	p->prepared = false;
 
 	return 0;
@@ -179,6 +145,7 @@ static int tc358762_unprepare(struct drm_panel *panel)
 
 static int rpi_touchscreen_write(struct mipi_dsi_device *dsi, u16 reg, u32 val)
 {
+	int ret;
 	u8 msg[] = {
 		reg,
 		reg >> 8,
@@ -188,83 +155,90 @@ static int rpi_touchscreen_write(struct mipi_dsi_device *dsi, u16 reg, u32 val)
 		val >> 24,
 	};
 
-	mipi_dsi_generic_write(dsi, msg, sizeof(msg));
-
-	return 0;
+	ret = mipi_dsi_generic_write(dsi, msg, sizeof(msg));
+	if (ret == sizeof(msg) + 4)
+		DRM_DEBUG("%s wrote 0x%08x to 0x%03x\n", __func__,
+			  val, reg);
+	else
+		DRM_ERROR("%s failed to write 0x%08x to 0x%03x: %d\n", __func__,
+			  val, reg, ret);
+	return ret;
 }
+
+static struct {
+	int addr;
+	u32 val;
+} tc358762_dsi_data[] = {
+	{ DSI_LANEENABLE, DSI_LANEENABLE_CLOCK | DSI_LANEENABLE_D0, },
+
+	{ PPI_D0S_CLRSIPOCOUNT, 0x05, },
+	{ PPI_D1S_CLRSIPOCOUNT, 0x05, },
+	{ PPI_D0S_ATMR, 0x00, },
+	{ PPI_D1S_ATMR, 0x00, },
+	{ PPI_LPTXTIMECNT, 0x03, },
+
+	{ SPICMR, 0x00, },
+	{ LCDCTRL, 0x00100150, },
+	{ SYSCTRL, 0x040f, },
+
+	{ PPI_STARTPPI, 0x01, },
+	{ DSI_STARTDSI, 0x01, },
+};
 
 static int tc358762_dsi_init(struct tc358762 *p)
 {
+	int ret;
 	struct mipi_dsi_device *dsi = p->dsi;
+	size_t i;
 
-	rpi_touchscreen_write(dsi, DSI_LANEENABLE,
-			      DSI_LANEENABLE_CLOCK |
-			      DSI_LANEENABLE_D0);
-
-	rpi_touchscreen_write(dsi, PPI_D0S_CLRSIPOCOUNT, 0x05);
-	rpi_touchscreen_write(dsi, PPI_D1S_CLRSIPOCOUNT, 0x05);
-	rpi_touchscreen_write(dsi, PPI_D0S_ATMR, 0x00);
-	rpi_touchscreen_write(dsi, PPI_D1S_ATMR, 0x00);
-	rpi_touchscreen_write(dsi, PPI_LPTXTIMECNT, 0x03);
-
-	rpi_touchscreen_write(dsi, SPICMR, 0x00);
-	rpi_touchscreen_write(dsi, LCDCTRL, 0x00100150);
-	rpi_touchscreen_write(dsi, SYSCTRL, 0x040f);
+	for (i = 0; i < ARRAY_SIZE(tc358762_dsi_data); i++) {
+		ret = rpi_touchscreen_write(dsi, tc358762_dsi_data[i].addr,
+					    tc358762_dsi_data[i].val);
+		if (ret < 0) {
+			DRM_ERROR("Failed to write %08x to reg %03x: %d\n",
+				  tc358762_dsi_data[i].val,
+				  tc358762_dsi_data[i].addr, ret);
+			return ret;
+		}
+		if (i == 8)
+			msleep(100);
+	}
 	msleep(100);
-
-	rpi_touchscreen_write(dsi, PPI_STARTPPI, 0x01);
-	rpi_touchscreen_write(dsi, DSI_STARTDSI, 0x01);
-	msleep(100);
-
 	return 0;
 }
 
 static int tc358762_prepare(struct drm_panel *panel)
 {
+	int ret;
 	struct tc358762 *p = to_tc358762(panel);
-	int err;
 
 	if (p->prepared)
 		return 0;
 
-	err = regulator_enable(p->supply);
-	if (err < 0) {
-		dev_err(panel->dev, "failed to enable supply: %d\n", err);
-		return err;
-	}
-
-	if (p->enable_gpio)
-		gpiod_direction_output(p->enable_gpio, 1);
-
-	if (p->desc && p->desc->delay.prepare)
-		msleep(p->desc->delay.prepare);
-
 	p->prepared = true;
 
-	return 0;
+	return ret;
 }
 
 static int tc358762_enable(struct drm_panel *panel)
 {
+	int ret;
 	struct tc358762 *p = to_tc358762(panel);
 
 	if (p->enabled)
 		return 0;
 
-	pr_debug("panel enable\n");
-
-	if (trigger_bridge) {
-		pr_debug("rpi_display_power_up");
-		rpi_display_screen_power_up();
-		trigger_bridge = 0;
-		msleep(100);
-		pr_debug("rpi_ft5406_start_polling");
-		rpi_ft5406_start_polling();
+	ret = regulator_enable(p->power_supply);
+	if (ret) {
+		DRM_ERROR("failed to enable power_supply: %d\n", ret);
+		return ret;
 	}
 
-	tc358762_dsi_init(p);
+	msleep(100);
 
-	rpi_display_set_bright(0xFF);
+	ret = tc358762_dsi_init(p);
+	if (ret)
+		return ret;
 
 	p->enabled = true;
 
@@ -294,8 +268,8 @@ static int tc358762_get_modes(struct drm_panel *panel, struct drm_connector *con
 }
 
 static int tc358762_get_timings(struct drm_panel *panel,
-				unsigned int num_timings,
-				struct display_timing *timings)
+					unsigned int num_timings,
+					struct display_timing *timings)
 {
 	struct tc358762 *p = to_tc358762(panel);
 	unsigned int i;
@@ -322,46 +296,7 @@ static const struct drm_panel_funcs tc358762_funcs = {
 	.get_timings = tc358762_get_timings,
 };
 
-static int tc358762_mipi_probe(struct mipi_dsi_device *dsi, const struct panel_desc *desc)
-{
-	struct tc358762 *panel;
-	struct device *dev = &dsi->dev;
-	int err;
-
-	panel = devm_kzalloc(dev, sizeof(*panel), GFP_KERNEL);
-	if (!panel)
-		return -ENOMEM;
-
-	panel->enabled = false;
-	panel->prepared = false;
-	panel->desc = desc;
-	panel->dev = dev;
-	panel->dsi = dsi;
-
-	panel->supply = devm_regulator_get(dev, "power");
-	if (IS_ERR(panel->supply))
-		return PTR_ERR(panel->supply);
-
-	panel->enable_gpio = devm_gpiod_get_optional(dev, "enable",
-							 GPIOD_OUT_LOW);
-	if (IS_ERR(panel->enable_gpio)) {
-		err = PTR_ERR(panel->enable_gpio);
-		dev_err(dev, "failed to request GPIO: %d\n", err);
-		return err;
-	}
-
-	drm_panel_init(&panel->base, dev, &tc358762_funcs, DRM_MODE_CONNECTOR_DSI);
-	panel->base.dev = dev;
-	panel->base.funcs = &tc358762_funcs;
-
-	drm_panel_add(&panel->base);
-
-	dev_set_drvdata(dev, panel);
-
-	return 0;
-}
-
-static int tc358762_remove(struct device *dev)
+static void tc358762_remove(struct device *dev)
 {
 	struct tc358762 *panel = dev_get_drvdata(dev);
 
@@ -369,7 +304,8 @@ static int tc358762_remove(struct device *dev)
 
 	tc358762_disable(&panel->base);
 
-	return 0;
+	if (panel->ddc)
+		put_device(&panel->ddc->dev);
 }
 
 static void tc358762_shutdown(struct device *dev)
@@ -387,86 +323,52 @@ struct bridge_desc {
 	unsigned int lanes;
 };
 
-static const struct bridge_desc tc358762_bridge = {
-	.desc = {
-		.modes = &tc358762_mode,
-		.num_modes = 1,
-		.bpc = 8,
-		.size = {
-			.width = 154,
-			.height = 86,
-		},
-		.bus_format = MEDIA_BUS_FMT_RGB888_1X24,
-	},
-	.flags = MIPI_DSI_MODE_VIDEO |
-		 MIPI_DSI_MODE_VIDEO_SYNC_PULSE |
-		 MIPI_DSI_MODE_LPM |
-		 MIPI_DSI_MODE_VIDEO_HSE,
-	.format = MIPI_DSI_FMT_RGB888,
-	.lanes = 1,
-};
-
-static const struct of_device_id dsi_of_match[] = {
-	{
-		.compatible = "raspberrypi,tc358762",
-		.data = &tc358762_bridge
-	},
-	{
-		/*
-		 * keep the unofficial 'rpi' vendor prefix for
-		 * compatibility with existing dtbs
-		 */
-		.compatible = "rpi,tc358762",
-		.data = &tc358762_bridge
-	},
-	{
-		/* sentinel */
-	}
-};
-MODULE_DEVICE_TABLE(of, dsi_of_match);
-
 static int tc358762_dsi_probe(struct mipi_dsi_device *dsi)
 {
-	const struct bridge_desc *desc;
-	const struct of_device_id *id;
-	const struct panel_desc *pdesc;
-	u32 val;
 	int err;
+	struct device *dev = &dsi->dev;
+	struct tc358762 *panel;
 
-	id = of_match_node(dsi_of_match, dsi->dev.of_node);
-	if (!id)
-		return -ENODEV;
+	panel = devm_kzalloc(dev, sizeof(*panel), GFP_KERNEL);
+	if (!panel)
+		return -ENOMEM;
 
-	if (of_device_is_compatible(dsi->dev.of_node, "rpi,tc358762"))
-		dev_warn(&dsi->dev,
-			 "Your DTB needs to be updated to use the official vendor prefix 'raspberrypi' rather than 'rpi'");
-
-	desc = id->data;
-
-	if (desc) {
-		dsi->mode_flags = desc->flags;
-		dsi->format = desc->format;
-		dsi->lanes = desc->lanes;
-		pdesc = &desc->desc;
-	} else {
-		pdesc = NULL;
+	panel->dsi = dsi;
+	panel->power_supply = devm_regulator_get(dev, "power");
+	if (IS_ERR(panel->power_supply)) {
+		return dev_err_probe(dev, PTR_ERR(panel->power_supply),
+				     "Failed to get power-supply regulator\n");
 	}
+	dev_info(dev, "Got power-supply regulator\n");
 
-	err = tc358762_mipi_probe(dsi, pdesc);
+	mipi_dsi_set_drvdata(dsi, panel);
 
-	if (err < 0)
+	dsi->lanes = 1;
+	dsi->format = MIPI_DSI_FMT_RGB888;
+	dsi->mode_flags = MIPI_DSI_MODE_VIDEO |
+			  MIPI_DSI_MODE_VIDEO_SYNC_PULSE |
+			  MIPI_DSI_MODE_LPM |
+			  MIPI_DSI_MODE_VIDEO_HSE;
+
+	panel->enabled = false;
+	panel->prepared = false;
+
+	panel->base.dev = dev;
+	panel->base.funcs = &tc358762_funcs;
+
+	drm_panel_init(&panel->base, dev, &tc358762_funcs, DRM_MODE_CONNECTOR_DSI);
+
+	drm_panel_add(&panel->base);
+
+	err = mipi_dsi_attach(dsi);
+	if (err < 0) {
+		if (err != -EPROBE_DEFER)
+			dev_err(dev, "mipi_dsi_attach failed: %d\n", err);
+
+		drm_panel_remove(&panel->base);
 		return err;
-
-	if (!of_property_read_u32(dsi->dev.of_node, "dsi,flags", &val))
-		dsi->mode_flags = val;
-
-	if (!of_property_read_u32(dsi->dev.of_node, "dsi,format", &val))
-		dsi->format = val;
-
-	if (!of_property_read_u32(dsi->dev.of_node, "dsi,lanes", &val))
-		dsi->lanes = val;
-
-	return mipi_dsi_attach(dsi);
+	}
+	return 0;
 }
 
 static void tc358762_dsi_remove(struct mipi_dsi_device *dsi)
@@ -485,6 +387,12 @@ static void tc358762_dsi_shutdown(struct mipi_dsi_device *dsi)
 	tc358762_shutdown(&dsi->dev);
 }
 
+static const struct of_device_id dsi_of_match[] = {
+	{ .compatible = "raspberrypi,tc358762", },
+	{ /* sentinel */ }
+};
+MODULE_DEVICE_TABLE(of, dsi_of_match);
+
 static struct mipi_dsi_driver tc358762_dsi_driver = {
 	.driver = {
 		.name = "bridge-tc358762-dsi",
@@ -494,28 +402,9 @@ static struct mipi_dsi_driver tc358762_dsi_driver = {
 	.remove = tc358762_dsi_remove,
 	.shutdown = tc358762_dsi_shutdown,
 };
+module_mipi_dsi_driver(tc358762_dsi_driver);
 
-static int __init tc358762_init(void)
-{
-	int err;
-
-	if (IS_ENABLED(CONFIG_DRM_MIPI_DSI)) {
-		err = mipi_dsi_driver_register(&tc358762_dsi_driver);
-		if (err < 0)
-			return err;
-	}
-
-	return 0;
-}
-module_init(tc358762_init);
-
-static void __exit tc358762_exit(void)
-{
-	if (IS_ENABLED(CONFIG_DRM_MIPI_DSI))
-		mipi_dsi_driver_unregister(&tc358762_dsi_driver);
-}
-module_exit(tc358762_exit);
-
+MODULE_AUTHOR("Markus Bauer <mb@karo-electronics.de>");
 MODULE_AUTHOR("Jerry <xbl@rock-chips.com>");
 MODULE_DESCRIPTION("DRM Driver for toshiba tc358762 Bridge");
 MODULE_LICENSE("GPL and additional rights");
