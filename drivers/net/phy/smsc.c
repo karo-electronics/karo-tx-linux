342df776b017273acfdbfe5cfe2dee8eaa82799d
Lothar Waßmann <LW@KARO-electronics.de>
2025-02-26 09:30:11 +0100

    net: phy: smsc: disable/re-enable interrupts upon suspend/resume

--- drivers/net/phy/smsc.c
+++ drivers/net/phy/smsc.c
@@ -203,9 +203,36 @@ static int smsc_phy_suspend(struct phy_device *phydev)
 {
 	struct device *dev = &phydev->mdio.dev;
 
+	if (phydev->suspended)
+		return 0;
+
 	/* do not power down PHY when PHY enable power/wakeup */
-	if (!device_may_wakeup(dev))
-		return genphy_suspend(phydev);
+	if (device_may_wakeup(dev))
+		return 0;
+
+	if (phy_interrupt_is_valid(phydev)) {
+		phydev->interrupts = PHY_INTERRUPT_DISABLED;
+		if (phydev->drv->config_intr)
+			phydev->drv->config_intr(phydev);
+	}
+
+	return genphy_suspend(phydev);
+}
+
+static int smsc_phy_resume(struct phy_device *phydev)
+{
+	genphy_resume(phydev);
+
+	if (!phydev->suspended)
+		return 0;
+
+	usleep_range(25000, 26000);
+
+	if (phy_interrupt_is_valid(phydev)) {
+		phydev->interrupts = PHY_INTERRUPT_ENABLED;
+		if (phydev->drv->config_intr)
+			phydev->drv->config_intr(phydev);
+	}
 
 	return 0;
 }
@@ -522,8 +549,8 @@ static struct phy_driver smsc_phy_driver[] = {
 	.get_strings	= smsc_get_strings,
 	.get_stats	= smsc_get_stats,
 
-	.suspend	= genphy_suspend,
-	.resume		= genphy_resume,
+	.suspend	= smsc_phy_suspend,
+	.resume		= smsc_phy_resume,
 }, {
 	/* This covers internal PHY (phy_id: 0x0007C0C3) for
 	 * LAN9500 (PID: 0x9500), LAN9514 (PID: 0xec00), LAN9505 (PID: 0x9505)
@@ -664,7 +691,7 @@ static struct phy_driver smsc_phy_driver[] = {
 	.set_tunable	= smsc_phy_set_tunable,
 
 	.suspend	= smsc_phy_suspend,
-	.resume		= genphy_resume,
+	.resume		= smsc_phy_resume,
 } };
 
 module_phy_driver(smsc_phy_driver);
